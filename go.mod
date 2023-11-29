559b0cede749f4cc3a67dbff939ea5ec1f6fa288
Windows/MacOS <xx@x>
2023-11-29 21:25:25 +0800

    up

--- go.mod
+++ go.mod
@@ -1,13 +1,13 @@
 module github.com/k0sf/gorm-sqlite3-n
 
-go 1.18
+go 1.21
 
 require (
 	github.com/mutecomm/go-sqlcipher/v4 v4.4.2
-	gorm.io/gorm v1.23.4
+	gorm.io/gorm v1.25.5
 )
 
 require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 )
--- test.go
+++ test.go
@@ -14,18 +14,25 @@ func main() {
 
 	key := url.QueryEscape("pass")
 	path := "./dbtest.db"
+	//path = "./5"
 	dsn := fmt.Sprintf("%s?_db_key=%s", path, key)
 
 	db, err = gorm.Open(wxSQLite3.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	type Users struct {
 		Id   int64  `json:"id"`
 		Name string `json:"name"`
 	}
+	db.AutoMigrate(&Users{})
 	var list []Users
+	for i := 0; i < 5; i++ {
+		u := Users{Name: fmt.Sprintf("%d", i)}
+		db.Table("users").Save(&u)
+	}
 
-	db.Table("users").Find(&list)
+	db.Table("users").Limit(3).Find(&list)
 	fmt.Println(list)
 }
